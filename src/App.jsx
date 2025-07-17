ed308c820a119fc896e6bc64bb312cfb1730670d
Saviskar <Saviskar>
2025-07-17 15:47:26 +0530

    feat: added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage";
@@ -6,12 +7,14 @@ import InternDashboard from "./pages/InternDashboard";
 
 function App() {
   return (
-    <>
-      {/* <LandingPage /> */}
-      {/* <LoginPage /> */}
-      {/* <RegistrationPage /> */}
-      <InternDashboard />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/dashboard" element={<InternDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
